578da4b29355906d151865c9a825e9612545532a
asma180 <asmaulhusna1527@gmail.com>
2019-12-22 12:43:24 +0600

    all done have fun

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+$(document) .ready(function(){
+    $(".video-play-btn").magnificPopup({
+        type: 'video',
+    });
+});
+
+
+/*(function($){
+    "use strict";
+
+    jQuery(document).ready(function($){
+
+        $(".video-play-btn").magnificPopup({
+            type: 'video',
+        });
+
+    });
+        jQuery(window).load(function(){
+
+        });
+}(jQuery));
+*/
